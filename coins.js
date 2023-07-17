375bb361b559c2152b97ddeb9fe916c8123f5583
JemsWaner <jemswaner@gmail.com>
2023-07-17 19:46:29 -0400

    I solve the swap button after many hours.

--- coins.js
+++ coins.js
@@ -1,6 +1,6 @@
 const MainSelect = document.getElementById("first-select");
 const SecondSelect = document.getElementById("second-select");
-const Button = document.getElementById("swap-button");
+const Button = document.querySelector(".swap-button");
 const NumberCounter = document.querySelector(".number-option");
 const Currency = document.querySelector(".show.currency");
 const ShortCurrency = document.querySelector(".short-currency");
@@ -28,6 +28,7 @@ function InsertingList(keys, values) {
     MainSelect.appendChild(option);
     SecondSelect.appendChild(option2);
   }
+
   let search = keys.indexOf("EUR");
   SecondSelect[search].setAttribute("selected", true);
   let defaults = keys.indexOf(SecondSelect.value);
@@ -44,7 +45,6 @@ async function GettingCountries() {
   let keysObjects = Object.keys(countryRates);
   let valuesObjects = Object.values(countryRates);
   InsertingList(keysObjects, valuesObjects);
-  console.log(keysObjects, valuesObjects);
 }
 
 GettingCountries();
@@ -58,14 +58,14 @@ NumberCounter.addEventListener("input", (e) => {
   return showNumber;
 });
 
-MainSelect.addEventListener("change", () => {
+MainSelect.addEventListener("change", async () => {
   coin = MainSelect.value;
-  GettingCountries();
+  await GettingCountries();
   Currency.textContent = `${NumberCounter.value} ${this.value} = ${showNumber}`;
 });
 
-SecondSelect.addEventListener("change", () => {
-  GettingCountries();
+SecondSelect.addEventListener("change", async () => {
+  await GettingCountries();
   Currency.textContent = `${NumberCounter.value} ${MainSelect.value} = ${showNumber}`;
   alert(secondCoin);
 });
@@ -73,3 +73,16 @@ SecondSelect.addEventListener("change", () => {
 console.log(
   "If the country's options aren't showing up, wait some seconds or reload the page again"
 );
+
+Button.addEventListener("click", () => {
+  //In the variable I save the first value, because I'll alter it later...So It's better to have the initial value at the moment of giving the value to the SecondSelect
+  let upValue = MainSelect.value;
+  coin = SecondSelect.value;
+  GettingCountries();
+  MainSelect.value = SecondSelect.value;
+  SecondSelect.value = upValue;
+
+  ///Here below I just want to make sure if the coin values are being change
+  console.log(MainSelect.value);
+  console.log(SecondSelect.value);
+});
