48e3b83f19afe78cccbdab07f8323c3e7d695426
JemsWaner <jemswaner@gmail.com>
2023-07-18 00:12:57 -0400

    Improvements on the accuracy of both exange coins

--- coins.js
+++ coins.js
@@ -6,7 +6,7 @@ const Currency = document.querySelector(".show.currency");
 const ShortCurrency = document.querySelector(".short-currency");
 let coin = "DOP";
 let secondCoin;
-
+let count = 0;
 ///////Putting a default text
 Currency.textContent = "1 DOP = 0.01607 EUR";
 ShortCurrency.textContent = 0.02;
@@ -28,17 +28,20 @@ function InsertingList(keys, values) {
     MainSelect.appendChild(option);
     SecondSelect.appendChild(option2);
   }
-
-  let search = keys.indexOf("EUR");
-  SecondSelect[search].setAttribute("selected", true);
+  if (count == 0) {
+    let search = keys.indexOf("EUR");
+    SecondSelect[search].setAttribute("selected", true);
+    count++;
+  }
   let defaults = keys.indexOf(SecondSelect.value);
   secondCoin = values[defaults];
   return secondCoin;
 }
+
 /* Here I call the API REST and the result is given as a parametrer to another function*/
-async function GettingCountries() {
+async function GettingCountries(coins) {
   let dataCall = await fetch(
-    `https://v6.exchangerate-api.com/v6/8479ed75352302d431c6ae3e/latest/${coin}`
+    `https://v6.exchangerate-api.com/v6/e361a23175b43c319c0a8570/latest/${coins}`
   );
   let convertObj = await dataCall.json();
   let countryRates = await convertObj.conversion_rates;
@@ -47,38 +50,39 @@ async function GettingCountries() {
   InsertingList(keysObjects, valuesObjects);
 }
 
-GettingCountries();
+GettingCountries(coin);
 
-function Numbers(e) {
+function Numbers() {
   let showNumber = Multiplication(NumberCounter.value, secondCoin);
   Currency.textContent = `${NumberCounter.value} ${
     MainSelect.value
   } = ${showNumber.toFixed(5)} ${SecondSelect.value}`;
   ShortCurrency.textContent = showNumber.toFixed(2);
-  return showNumber;
 }
+///The event that changes the numbers
 NumberCounter.addEventListener("input", Numbers);
 
 MainSelect.addEventListener("change", async () => {
   coin = MainSelect.value;
-  await GettingCountries();
+  await GettingCountries(coin);
   Numbers();
 });
 
 SecondSelect.addEventListener("change", async () => {
-  await GettingCountries();
+  await GettingCountries(coin);
   Numbers();
-  alert(secondCoin);
 });
 
-Button.addEventListener("click", () => {
+Button.addEventListener("click", async () => {
   //In the variable I save the first value, because I'll alter it later...So It's better to have the initial value at the moment of giving the value to the SecondSelect
   let upValue = MainSelect.value;
-  coin = SecondSelect.value;
-  GettingCountries();
   MainSelect.value = SecondSelect.value;
   SecondSelect.value = upValue;
+
+  coin = MainSelect.value;
+  await GettingCountries(coin);
   Numbers();
+
   ///Here below I just want to make sure if the coin values are being change
   console.log(MainSelect.value);
   console.log(SecondSelect.value);
